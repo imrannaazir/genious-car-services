12257de888bf83e4112e240cef0abff3e641dea9
imrannaazir <imrannaaziremon@gmail.com>
2022-04-15 10:56:56 +0600

    sign up and log in page created

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path='/home' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/service/:serviceId' element={<ServiceDetails />} />
+        <Route path='login' element={<Login />} />
+
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
       <Footer />
 
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,78 @@
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
+    const confirmPasswordRef = useRef('')
+    const handleSubmit = event => {
+        event.preventDefault()
+        const email = emailRef.current.value
+        const password = passwordRef.current.value
+        const confirmPassword = confirmPasswordRef.current.value
+        console.log(email, password, confirmPassword);
+    }
+    return (
+        <div className=' w-[360px] md:w-[400px] mx-auto text-lg text-center shadow-lg rounded-lg py-12 my-10 bg-white flex flex-col gap-6 '>
+            <p className=' text-2xl font-bold text-gray-600'>Join Ema-John</p>
+            <form onSubmit={handleSubmit} className='flex flex-col  gap-6'>
+                <input
+                    ref={emailRef}
+                    type="email"
+                    name="email"
+                    id="" placeholder='Enter email'
+                    className=' border-2 py-2 w-[350px] rounded-md pl-6 block mx-auto border-orange-400'
+                    required
+                />
+                <input
+                    ref={passwordRef}
+                    type="password"
+                    name="password"
+                    id="" placeholder='Create password'
+                    className=' border-2 py-2 w-[350px] rounded-md pl-6 block mx-auto border-orange-400'
+                    required
+                />
+                <input
+                    ref={confirmPasswordRef}
+                    type="password"
+                    name="password"
+                    id="" placeholder='Confirm password'
+                    className=' border-2 py-2 w-[350px] rounded-md pl-6 block mx-auto border-orange-400'
+                    required
+                />
+                <p className='text-left ml-6 text-red-600'>f</p>
+                <button
+
+                    type="submit"
+                    className=' border-2 py-2 w-[350px] rounded-md pl-6 block mx-auto hover: border-orange-400 bg-orange-400 text-white font-bold'
+                >
+                    Sign up
+                </button>
+            </form>
+            <div className="flex justify-evenly space-x-2 w-64 mt-4 mx-auto">
+                <span
+                    className="bg-gray-300 h-px flex-grow t-2 relative top-2">
+                </span>
+                <span
+                    className="flex-none uppercase text-xs text-gray-400 font-semibold">or
+                </span>
+                <span
+                    className="bg-gray-300 h-px flex-grow t-2 relative top-2">
+
+                </span>
+            </div>
+            <button
+
+                className='flex items-center border-2 py-2 w-[350px] mx-auto rounded-md pl-6 gap-8'>
+                <img
+                    className='w-5' src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" alt="" />
+                Join with Google</button>
+            <p>
+                Haven't a account?<Link className=' underline hover:text-orange-400' to='/login'>Login</Link>
+            </p>
+
+        </div>
+    );
+};
+
+export default SignUp;
